1238ca7d97294597b026595bd3b28e2cbe083ca9
Manav Daggula <manavdaggula@gmail.com>
2021-10-12 21:05:22 +0530

    started menu page dev.

--- src/parkingManagementSystem/MenuPage.java
+++ src/parkingManagementSystem/MenuPage.java
@@ -1,28 +1,29 @@
 package parkingManagementSystem;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+//import javax.swing.JPanel;
 import javax.swing.JLabel;
 
-import java.awt.Image;
+//import java.awt.Image;
 
 import javax.swing.ImageIcon;
 
 public class MenuPage{
 
-    JFrame menuFrame;
-    JPanel MenuPanel;
-    JLabel imageLabel;
+    public JFrame menuFrame;
+    //JPanel MenuPanel;
+    public JLabel imageLabel;
     MenuPage(){
-        imageLabel = new JLabel();
-        imageLabel.setIcon(new ImageIcon(new ImageIcon("/home/manav/home/manav/Documents/EmptyParkingLot.jpg").getImage().getScaledInstance(500, 500, Image.SCALE_DEFAULT)));
+    	menuFrame.setLayout(null);
+    	menuFrame.setBounds(250,250,600,600);
+    	imageLabel = new JLabel("");
+        imageLabel.setIcon(new ImageIcon(ClassLoader.getSystemResource("/home/manav/home/manav/Documents/EmptyParkingLot.jpg")));
         imageLabel.setSize(500,500);
         imageLabel.setLocation(0,0);
         menuFrame.add(imageLabel);
         
-        menuFrame.setLayout(null);
         menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        menuFrame.setBounds(250,250,600,600);
+        
         menuFrame.setVisible(true);
     }
     public static void main(String args[]){
